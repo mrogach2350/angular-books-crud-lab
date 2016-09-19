3e15075d280e70998b0dd76f6b12be2ff4fda362
mrogach2350 <m.rogach@gmail.com>
2016-09-19 11:03:44 -0700

    updates routing

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ angular
   function config( $routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'templates/books/index',
+        templateUrl: '/templates/books/index.html',
         controllerAs: 'booksIndexCtrl',
         controller: 'BooksIndexController'
       })
       .when('/books/:id', {
-        templateUrl: 'templates/books/show',
+        templateUrl: '/templates/books/show.html',
         controllerAs: 'booksShowCtrl',
         controller: 'BooksShowController'
       });
